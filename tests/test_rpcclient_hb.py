52bede3ffeb508ae0b891fea5f01e32e437a3f7f
Konstantinos Panayiotou <klpanagi@gmail.com>
2020-07-06 16:58:13 +0300

    Some tests

--- tests/test_rpcclient_hb.py
+++ tests/test_rpcclient_hb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from commlib_py.transports.amqp import Publisher, ConnectionParameters
+from commlib_py.transports.amqp import RPCServer, RPCClient, ConnectionParameters
 import time
 from threading import Thread
 
@@ -8,27 +8,36 @@ from threading import Thread
 HB_TIMEOUT = 1
 
 
-def thread_runner(p):
+def thread_runner(c):
     data = {'state': 0}
     while True:
-        p.publish(data)
+        c.call(data)
         time.sleep(HB_TIMEOUT * 10)
+
+def on_request(msg, meta):
+    print(msg)
+    return msg
 
 
 if __name__ == '__main__':
-    topic_name = 'testtopic'
+    rpc_name = 'testrpc'
     conn_params = ConnectionParameters()
     conn_params.credentials.username = 'testuser'
     conn_params.credentials.password = 'testuser'
     conn_params.host = 'r4a-platform.ddns.net'
     conn_params.port = 5782
     conn_params.heartbeat_timeout = HB_TIMEOUT  ## Seconds
-    p = Publisher(conn_params=conn_params,
-                  topic=topic_name)
-    t = Thread(target=thread_runner, args=(p,))
+    s = RPCServer(conn_params=conn_params,
+                  rpc_name=rpc_name,
+                  on_request=on_request)
+    s.run()
+    time.sleep(1)
+    c = RPCClient(conn_params=conn_params,
+                  rpc_name=rpc_name)
+    t = Thread(target=thread_runner, args=(c,))
     t.daemon = True
     t.start()
     data = {'state': 0}
     while True:
-        p.publish(data)
+        c.call(data)
         time.sleep(HB_TIMEOUT * 10)
