ee5c60e0731d2a474d5958d9d245daa12e0d0507
Konstantinos Panayiotou <klpanagi@gmail.com>
2020-08-01 18:52:34 +0300

    redis and amqp pubsub tests

---
+++ tests/test_redis_action.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import commlib.transports.amqp as acomm
+import commlib.transports.redis as rcomm
+import time
+
+
+def on_goal(goalh):
+    count = 0
+    while True:
+        time.sleep(1)
+        if goalh.cancel_event.is_set():
+            return {'result': 0}
+        count += 1
+        if count > 4:
+            return {'result': 1}
+
+
+def run_redis():
+    print()
+    print('-----------------------------------------------------------------')
+    print('Running REDIS ACtion Test...')
+    print('-----------------------------------------------------------------')
+    action_name = 'testaction'
+    conn_params = rcomm.UnixSocketConnectionParameters()
+
+    action = rcomm.ActionServer(conn_params=conn_params,
+                                action_name=action_name,
+                                on_goal=on_goal)
+    action.run()
+
+    ac = rcomm.ActionClient(conn_params=conn_params,
+                            action_name=action_name)
+    goal_data = {'a': 1, 'b': 'test'}
+
+    resp = ac.send_goal(goal_data)
+    _goal_id = resp['goal_id']
+    if _goal_id is None:
+        pass
+    print('Send-Goal Response: {}'.format(resp))
+    time.sleep(1)
+    resp = ac.get_result(_goal_id)
+    print('Get-Result Response: {}'.format(resp))
+    time.sleep(1)
+    resp = ac.cancel_goal(_goal_id)
+    print('Cancel-Goal Response: {}'.format(resp))
+    time.sleep(1)
+    resp = ac.get_result(_goal_id)
+    print('Get-Result Response: {}'.format(resp))
+
+    resp = ac.send_goal(goal_data)
+    _goal_id = resp['goal_id']
+    print('Send-Goal Response: {}'.format(resp))
+    time.sleep(5)
+    resp = ac.get_result(_goal_id)
+    print('Get-Result Response: {}'.format(resp))
+    action.stop()
+
+
+if __name__ == '__main__':
+    run_redis()
+    # run_amqp()
+    print('==========================================')
+    print('================END OF TEST===============')
+    print('==========================================')
