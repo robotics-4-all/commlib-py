dc50965aabf10017659f5f2e01337b1cd3862d5b
Konstantinos Panayiotou <klpanagi@gmail.com>
2020-12-27 19:59:18 +0200

    Do not expose MQTT port

--- examples/brokers_docker/launch_rabbitmq_docker.sh
+++ examples/brokers_docker/launch_rabbitmq_docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -it --hostname rabbit -p 15672:15672 -p 5672:5672 -p 1883:1883 rabbitmq:3-management
+docker run --rm -it --hostname rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
