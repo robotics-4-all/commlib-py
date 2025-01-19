385adc3ce4bf472765956f13047ffbf289ff542b
Konstantinos Panayiotou <klpanagi@gmail.com>
2025-01-19 16:27:29 +0200

    Error handling + TopicDataProcessor

--- commlib/aggregation.py
+++ commlib/aggregation.py
@@ -7,6 +7,57 @@ from commlib.node import Node
 aggregation_logger = None
 
 
+class TopicMessageProcessor:
+    def __init__(self, broker_params: BaseConnectionParameters,
+                 input_topic: List[str], output_topic: str,
+                 data_processors: List[callable] = []):
+        self.broker_params = broker_params
+        self.input_topic = input_topic
+        self.output_topic = output_topic
+        self.data_processors = data_processors  # List of functions to process incoming data
+
+        self.node = Node(node_name="TopicMerge",
+                         connection_params=self.broker_params,
+                         debug=False, heartbeats=False)
+
+    @classmethod
+    def logger(cls) -> logging.Logger:
+        global aggregation_logger
+        if aggregation_logger is None:
+            aggregation_logger = logging.getLogger(__name__)
+        return aggregation_logger
+
+    @property
+    def log(self):
+        return self.logger()
+
+    def create_subscriptions(self):
+        _clb = functools.partial(self.on_msg_internal, self.data_processors)
+        self.node.create_psubscriber(topic=self.input_topic, on_message=_clb)
+
+    def create_publisher(self):
+        self.pub = self.node.create_mpublisher()
+
+    def on_msg_internal(self, processors: Dict[str, callable],
+                        payload: Dict[str, Any], topic: str):
+        for proc in processors:
+            try:
+                payload = proc(payload)
+                if not isinstance(payload, dict):
+                    self.log.warning("Processor did not return a dict")
+                    continue
+                self.pub.publish(topic=self.output_topic, msg=payload)
+                self.log.info(f"Processed message: {payload}")
+            except Exception as e:
+                self.log.error(f"Error processing message: {e}")
+                continue
+
+    def start(self):
+        self.create_publisher()
+        self.create_subscriptions()
+        self.node.run_forever()
+
+
 class TopicMerge:
     def __init__(self, broker_params: BaseConnectionParameters,
                  input_topics: List[str], output_topic: str,
@@ -19,6 +70,7 @@ class TopicMerge:
         self.node = Node(node_name="TopicMerge",
                          connection_params=self.broker_params,
                          debug=False, heartbeats=False)
+
     @classmethod
     def logger(cls) -> logging.Logger:
         global aggregation_logger
@@ -47,6 +99,9 @@ class TopicMerge:
         for proc in processors:
             try:
                 payload = proc(payload)
+                if not isinstance(payload, dict):
+                    self.log.warning("Processor did not return a dict")
+                    continue
                 self.pub.publish(topic=self.output_topic, msg=payload)
                 self.log.info(f"Processed message: {payload}")
             except Exception as e:
