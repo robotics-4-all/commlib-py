50c3a6fb14908e2b5650842c75d594e286c59785
Konstantinos Panayiotou <klpanagi@gmail.com>
2025-12-29 15:39:45 +0200

    Adds performance benchmark script

--- scripts/pubsub_perf.py
+++ scripts/pubsub_perf.py
@@ -18,7 +18,17 @@ from rich import box
 
 from commlib.node import Node
 from commlib.msg import PubSubMessage
+from commlib.msg import PubSubMessage
 from commlib.utils import get_timestamp_ns
+
+try:
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib.animation as animation
+    import numpy as np
+    HAS_MATPLOTLIB = True
+except ImportError:
+    HAS_MATPLOTLIB = False
 
 console = Console()
 
@@ -64,6 +74,48 @@ class ResourceMonitor:
         }
 
 
+class RealTimePlotter:
+    def __init__(self, data_queue: multiprocessing.Queue):
+        self.data_queue = data_queue
+        self.xs = []
+        self.ys = []
+        self.zs = []
+        self.cs = []  # Colors (Size)
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111, projection='3d')
+        self.sc = None
+
+    def update(self, frame):
+        try:
+            while not self.data_queue.empty():
+                data = self.data_queue.get_nowait()
+                if data is None:
+                    return
+                self.xs.append(data['num_subs'])
+                self.ys.append(data['target_freq'])
+                self.zs.append(data['avg_latency'])
+                self.cs.append(data['data_size_kb'])
+        except Exception:
+            pass
+
+        if not self.xs:
+            return
+
+        self.ax.clear()
+        self.ax.set_xlabel('Subscribers')
+        self.ax.set_ylabel('Frequency (Hz)')
+        self.ax.set_zlabel('Latency (ms)')
+        self.ax.set_title('Real-time Performance Metrics')
+        
+        # Scatter plot
+        self.sc = self.ax.scatter(self.xs, self.ys, self.zs, c=self.cs, cmap='viridis', marker='o')
+        # self.fig.colorbar(self.sc, label='Data Size (KB)') # Colorbar might be tricky to update repeatedly
+
+    def start(self):
+        ani = animation.FuncAnimation(self.fig, self.update, interval=1000)
+        plt.show()
+
+
 class PubSubPerfTest:
     def __init__(self, broker: str, topic: str = "perf.test"):
         self.broker = broker
@@ -98,7 +150,7 @@ class PubSubPerfTest:
         self.latencies.append(latency_ms)
         self.msg_count += 1
 
-    def run_single_size(self, num_subs: int, size_kb: int, num_messages: int, node: Node, pub: Any):
+    def run_single_size(self, num_subs: int, size_kb: int, target_freq: int, num_messages: int, node: Node, pub: Any):
         # Reset counters for this data size
         self.latencies = []
         self.msg_count = 0
@@ -110,9 +162,11 @@ class PubSubPerfTest:
         
         pub_start_ns = get_timestamp_ns()
         pub_start = time.time()
-        for _ in range(num_messages):
+        for i in range(num_messages):
             msg = PerfMessage(data=data_str, ts=get_timestamp_ns())
             pub.publish(msg)
+            if target_freq > 0 and i < num_messages - 1:
+                time.sleep(1.0 / target_freq)
         pub_end = time.time()
         
         expected_total_msgs = num_messages * num_subs
@@ -138,6 +192,7 @@ class PubSubPerfTest:
         return {
             "num_subs": num_subs,
             "data_size_kb": size_kb,
+            "target_freq": target_freq,
             "avg_latency": avg_latency,
             "pub_freq": pub_freq,
             "recv_freq": recv_freq,
@@ -146,7 +201,7 @@ class PubSubPerfTest:
             "success_rate": success_rate
         }
 
-    def _worker_run_subs(self, num_subs: int, sizes_list: List[int], num_messages: int, results_queue: multiprocessing.Queue):
+    def _worker_run_subs(self, num_subs: int, sizes_list: List[int], freqs_list: List[int], num_messages: int, results_queue: multiprocessing.Queue):
         for size_kb in sizes_list:
             # Start Node for this run
             node = Node(
@@ -157,17 +212,20 @@ class PubSubPerfTest:
                 node.create_subscriber(topic=self.topic, msg_type=PerfMessage, on_message=self.on_message)
             pub = node.create_publisher(topic=self.topic, msg_type=PerfMessage)
             node.run(wait=True)
+            time.sleep(1.0)
             
-            res = self.run_single_size(num_subs, size_kb, num_messages, node, pub)
-            results_queue.put(res)
+            for target_freq in freqs_list:
+                res = self.run_single_size(num_subs, size_kb, target_freq, num_messages, node, pub)
+                results_queue.put(res)
+                time.sleep(0.5)
             
             node.stop(wait=True)
-            time.sleep(0.5) # Cool down between sizes
+            time.sleep(1.0) # Cool down between sizes
         
         # Signal completion for this sub block
         results_queue.put(None)
 
-    def run_benchmark(self, subs_list: List[int], sizes_list: List[int], num_messages: int):
+    def run_benchmark(self, subs_list: List[int], sizes_list: List[int], freqs_list: List[int], num_messages: int, plot_queue: multiprocessing.Queue = None):
         results = []
         
         with Progress(
@@ -179,14 +237,14 @@ class PubSubPerfTest:
             console=console
         ) as progress:
             
-            total_steps = len(subs_list) * len(sizes_list)
+            total_steps = len(subs_list) * len(sizes_list) * len(freqs_list)
             overall_task = progress.add_task("[yellow]Benchmarking...", total=total_steps)
             
             for num_subs in subs_list:
                 results_queue = multiprocessing.Queue()
                 process = multiprocessing.Process(
                     target=self._worker_run_subs,
-                    args=(num_subs, sizes_list, num_messages, results_queue)
+                    args=(num_subs, sizes_list, freqs_list, num_messages, results_queue)
                 )
                 process.start()
                 
@@ -195,7 +253,9 @@ class PubSubPerfTest:
                     if res is None: # Sub block finished
                         break
                     results.append(res)
-                    progress.update(overall_task, advance=1, description=f"[cyan]Subs={res['num_subs']}, Size={res['data_size_kb']}KB")
+                    if plot_queue:
+                        plot_queue.put(res)
+                    progress.update(overall_task, advance=1, description=f"[cyan]Subs={res['num_subs']}, Size={res['data_size_kb']}KB, Freq={res['target_freq']}Hz")
                 
                 process.join()
                 time.sleep(1.0) # Cool down between subscriber counts
@@ -206,17 +266,20 @@ class PubSubPerfTest:
 def main():
     parser = argparse.ArgumentParser(description="PubSub Performance Test")
     parser.add_argument("--broker", type=str, required=True, choices=["redis", "amqp", "mqtt", "kafka"], help="Broker to use")
-    parser.add_argument("--num-messages", type=int, default=1000, help="Number of messages per data size")
+    parser.add_argument("--num-messages", type=int, default=100, help="Number of messages per data size")
     parser.add_argument("--max-size-kb", type=int, default=512, help="Maximum data size in KB to test")
     parser.add_argument("--start-size-kb", type=int, default=2, help="Starting data size in KB")
     parser.add_argument("--step-size-kb", type=int, default=2, help="Step size in KB (for linear strategy)")
     parser.add_argument("--size-strategy", type=str, default="predefined", choices=["linear", "exponential", "predefined"], help="Strategy for increasing data size")
-    parser.add_argument("--max-subs", type=int, default=32, help="Maximum number of subscribers to test")
+    parser.add_argument("--max-subs", type=int, default=16, help="Maximum number of subscribers to test")
     parser.add_argument("--start-subs", type=int, default=1, help="Starting number of subscribers")
     parser.add_argument("--step-subs", type=int, default=2, help="Step for subscriber count (for linear strategy)")
     parser.add_argument("--subs-strategy", type=str, default="exponential", choices=["linear", "exponential", "predefined"], help="Strategy for increasing subscriber count")
     parser.add_argument("--subs", type=int, nargs="+", default=[1, 5, 10], help="Predefined list of subscriber counts (used if strategy is 'predefined')")
     parser.add_argument("--topic", type=str, default="perf.test", help="Topic to use")
+    parser.add_argument("--start-freq-exp", type=int, default=0, help="Starting exponent for frequency (2^x)")
+    parser.add_argument("--max-freq-exp", type=int, default=7, help="Maximum exponent for frequency (2^x)")
+    parser.add_argument("--plot", action="store_true", help="Enable real-time 3D plotting")
     
     args = parser.parse_args()
     
@@ -226,7 +289,8 @@ def main():
         f"Messages/Size: [green]{args.num_messages}[/green]\n"
         f"Max Size: [green]{args.max_size_kb} KB[/green] (Strategy: [green]{args.size_strategy}[/green])\n"
         f"Max Subs: [green]{args.max_subs}[/green] (Strategy: [green]{args.subs_strategy}[/green])\n"
-        f"Logic: [italic]Fixed Subs -> Variable Data Size[/italic]",
+        f"Freq Range: [green]2^{args.start_freq_exp} - 2^{args.max_freq_exp} Hz[/green]\n"
+        f"Logic: [italic]Fixed Subs -> Variable Data Size -> Variable Freq[/italic]",
         box=box.DOUBLE
     ))
 
@@ -274,12 +338,32 @@ def main():
         sizes_list.append(args.max_size_kb)
         sizes_list.sort()
     
-    results = tester.run_benchmark(subs_list, sizes_list, args.num_messages)
+    # Generate Frequency List
+    freqs_list = [2**x for x in range(args.start_freq_exp, args.max_freq_exp + 1)]
+
+    plot_queue = None
+    plot_process = None
+    if args.plot:
+        if not HAS_MATPLOTLIB:
+            console.print("[red]matplotlib not installed. Plotting disabled.[/red]")
+        else:
+            plot_queue = multiprocessing.Queue()
+            plotter = RealTimePlotter(plot_queue)
+            plot_process = multiprocessing.Process(target=plotter.start)
+            plot_process.start()
+
+    try:
+        results = tester.run_benchmark(subs_list, sizes_list, freqs_list, args.num_messages, plot_queue)
+    finally:
+        if plot_process:
+            plot_process.terminate()
+            plot_process.join()
 
     # Final Table
     table = Table(title=f"Benchmark Results - {args.broker.upper()}", box=box.ROUNDED, header_style="bold magenta")
     table.add_column("Subs", justify="right", style="cyan")
     table.add_column("Size (KB)", justify="right", style="cyan")
+    table.add_column("Target Hz", justify="right", style="magenta")
     table.add_column("Lat (ms)", justify="right", style="green")
     table.add_column("Pub (Hz)", justify="right", style="yellow")
     table.add_column("Recv (Hz)", justify="right", style="yellow")
@@ -291,6 +375,7 @@ def main():
         table.add_row(
             str(r['num_subs']),
             str(r['data_size_kb']),
+            str(r['target_freq']),
             f"{r['avg_latency']:.2f}",
             f"{r['pub_freq']:.2f}",
             f"{r['recv_freq']:.2f}",
